545504221c0c9ffd278d89365042a79bc91d4015
Michael Hamilton <98mikeham@gmail.com>
2019-05-16 21:48:14 +0100

    update components array to dots

--- src/app/dots/Dots.jsx
+++ src/app/dots/Dots.jsx
@@ -4,15 +4,15 @@ import styles from '../../themes/index.styles.scss';
 
 export default class Dots extends React.Component {
   returnDots() {
-    let components = [];
+    let dots = [];
     for (let x = 0; x < this.props.number; x++) {
       const classNames = [styles.dot];
       if(this.props.current === x) {
         classNames.push(styles.dotActive)
       }
-      components.push(<div className={classnames(classNames)} onClick={() => this.props.onClick(x)}/>);
+      dots.push(<div className={classnames(classNames)} onClick={() => this.props.onClick(x)}/>);
     }
-    return components;
+    return dots;
   }
 
   render() {
